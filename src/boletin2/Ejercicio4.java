e84de9c1ec2a4e141dc26bdf1847d1d7f99f0ffd
mdominguezg <mdominguezg@106-06.info2>
2022-10-11 12:45:41 +0200

    commit

--- src/boletin2/Ejercicio4.java
+++ src/boletin2/Ejercicio4.java
@@ -1,10 +1,69 @@
 package boletin2;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// bloque de variables
+		String primera,segunda;					// se guardaran el valor obtenido dsp de la tirada 
+		// creamos scanner y preguntadmos
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Introduzca el valor obtenido en la 1º (Con letras): ");
+		primera=sc.next();
+		System.out.println("Introduzca el valor obtenido en la 2º( Con letras): ");
+		segunda=sc.next();
+		int num1 = switch (primera){			// aqui se guardaran los numeros obtenidos en la tirada
+		case "UNO"->{
+			yield 1;	//corresponde con el numero 1
+		}
+		case "DOS"->{
+			yield 2;	//corresponde con el numero 2
+		}
+		case "TRES"->{
+			yield 3;	//corresponde con el numero 3
+		}
+		case "CUATRO"->{
+			yield 4;	//corresponde con el numero 4
+		}
+		case "CINCO"->{
+			yield 5;	//corresponde con el numero 5
+		}
+		case "SEIS"->{
+			yield 6;	//corresponde con el numero 6
+		}
+		default -> {
+			System.out.print("Unexpected value: " + primera);
+			yield 0;
+		}
+		};
+		int num2 = switch (segunda){			// aqui se guardaran los numeros obtenidos en la tirada
+		case "UNO"->{
+			yield 1;	//corresponde con el numero 1
+		}
+		case "DOS"->{
+			yield 2;	//corresponde con el numero 2
+		}
+		case "TRES"->{
+			yield 3;	//corresponde con el numero 3
+		}
+		case "CUATRO"->{
+			yield 4;	//corresponde con el numero 4
+		}
+		case "CINCO"->{
+			yield 5;	//corresponde con el numero 5
+		}
+		case "SEIS"->{
+			yield 6;	//corresponde con el numero 6
+		}
+		default -> {
+			System.out.print("Unexpected value: " + segunda);
+			yield 0;
+		}
+	};
+	System.out.print("El valor de la tirada es de: "+(num1+num2));
+	sc.close();
 	}
-
 }
+
+
---
+++ src/boletin2/Ejercicio5.java
@@ -0,0 +1,12 @@
+package boletin2;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		//bloque de variables
+		String carnet;			//el tipo de carnet que conduce
+		
+
+	}
+
+}
